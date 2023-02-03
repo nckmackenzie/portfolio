3da67838d4c73188d2d824ce652647a7cbd13195
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-03 16:20:04 +0300

    Home page Navigation component

---
+++ src/components/layout/Navigation.jsx
@@ -0,0 +1,44 @@
+import { useTheme } from '../../context/ThemeProvider';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { links } from '../../data/data';
+import NavItem from './NavItem';
+
+function Navigation() {
+  const { textClr, isDarkMode, toogleTheme } = useTheme();
+  return (
+    <header className="py-2 lg:p-4 flex items-center justify-between">
+      <Link to="/">
+        <h2
+          className={`font-headings ${
+            isDarkMode ? 'text-primary-400' : 'text-accent-400'
+          }  text-xl`}
+        >
+          Mackenzie
+        </h2>
+      </Link>
+      <div className="center gap-2 lg:gap-8">
+        <ul className="hidden md:center gap-4">
+          {links?.length > 0 &&
+            links.map(link => <NavItem key={link.name} {...link} />)}
+        </ul>
+        <button
+          className={`inline-flex md:hidden ${textClr} border border-solid px-2 py-1 transition-all ${
+            isDarkMode ? 'border-primary-100' : 'border-accent-200'
+          } rounded hover:scale-105`}
+        >
+          <AiOutlineMenu />
+        </button>
+        <button
+          className={`inline-flex px-2 py-1 text-lg ${textClr}`}
+          onClick={toogleTheme}
+        >
+          {isDarkMode ? <MdLightMode /> : <MdDarkMode />}
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Navigation;
