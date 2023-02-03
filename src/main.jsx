4ab6cb94fc098ed63a4bca57287c1ed4e74f39e6
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-03 12:59:04 +0300

    set RouterProvider and ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
+import { ThemeProvider } from './context/ThemeProvider';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
