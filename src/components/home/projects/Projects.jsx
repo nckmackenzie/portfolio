d892b9bc565ec2e8ecfe5a1701a113eef9fd9d81
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-06 16:45:18 +0300

    Projects component initial start

---
+++ src/components/home/projects/Projects.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+// import { useTheme } from '../../../context/ThemeProvider';
+import { projects } from '../../../data/data';
+import { Section } from '../../ui';
+import Filters from './Filters';
+
+const projectCategories = projects.map(project => project.type);
+const categories = [...new Set(['All', ...projectCategories])];
+function Projects() {
+  //   const { textClr } = useTheme();
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  return (
+    <Section
+      title="Portfolio"
+      description="Projects i have worked on before."
+      lgPadding
+    >
+      <Filters
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+    </Section>
+  );
+}
+
+export default Projects;
