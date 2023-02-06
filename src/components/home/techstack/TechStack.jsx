34b22d69fee4e0d7ff6e04e0c702113c5dfc5e1a
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-06 13:03:30 +0300

    change description for TechStack section

--- src/components/home/techstack/TechStack.jsx
+++ src/components/home/techstack/TechStack.jsx
@@ -7,8 +7,7 @@ function TechStack() {
     <Section
       title="My Tech Stack"
       lgPadding
-      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, magni
-  adipisci optio placeat iste quasi."
+      description="Technologies i have been working with lately."
     >
       <div className="mt-8 md:mt-12 grid grid-cols-2 md:grid-cols-5 gap-4 md:gap-8">
         {myTechStacks.length > 0 &&
