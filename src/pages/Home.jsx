6fe18738afdb2d7a7e73c00745338b02e5740758
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-07 12:10:27 +0300

    import projects from data and pass as prop to Projects component,add Testimonials component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,12 @@ import Hero from '../components/home/hero/Hero';
 import TechStack from '../components/home/techstack/TechStack';
 import Services from '../components/home/services/Services';
 import Projects from '../components/home/projects/Projects';
+import Testimonials from '../components/home/testimonials/Testimonials';
+import { projects } from '../data/data';
 
 function Home() {
   const { appBg } = useTheme();
+
   return (
     <div className={`min-h-screen ${appBg}`}>
       <div className="container">
@@ -14,7 +17,8 @@ function Home() {
         <Hero />
         <TechStack />
         <Services />
-        <Projects />
+        <Projects projects={projects} />
+        <Testimonials projects={projects} />
       </div>
     </div>
   );
