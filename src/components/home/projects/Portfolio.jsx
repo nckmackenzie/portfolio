a3f648616c4b7a6618bc2821d74755153f3ea456
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-07 10:20:26 +0300

    add to prop,wrap card in Link component

--- src/components/home/projects/Portfolio.jsx
+++ src/components/home/projects/Portfolio.jsx
@@ -1,25 +1,39 @@
+import { Link } from 'react-router-dom';
 import { useTheme } from '../../../context/ThemeProvider';
 import { Card } from '../../ui';
+import Tag from './Tag';
 
-function Portfolio({ icon, project, description }) {
-  const { paragraphText } = useTheme();
+function Portfolio({ icon, project, description, tags, to }) {
+  const { paragraphText, isDarkMode } = useTheme();
   return (
-    <Card className="w-full bg-slate-800">
-      <div className="h-[9rem]">
-        <img
-          src={icon.small}
-          alt="Project description"
-          className="max-w-full h-full w-full"
-        />
-      </div>
-      <div className="p-2 ">
-        <h5 className="text-primary-100 mb-2">{project}</h5>
-        <p className={`text-xs ${paragraphText} mb-2`}>{description}</p>
-        <span className="text-[10px] bg-primary-300 text-accent-400 inline-block px-1">
-          HTML
-        </span>
-      </div>
-    </Card>
+    <Link to={`projects/${to}`} className="transition-all hover:scale-105">
+      <Card
+        className={`w-full ${isDarkMode ? 'bg-slate-800' : 'bg-slate-100'}`}
+      >
+        <div className="h-[9rem]">
+          <img
+            src={icon.small}
+            alt="Project description"
+            className="max-w-full h-full w-full"
+          />
+        </div>
+        <div className="p-2">
+          <h5
+            className={`${
+              isDarkMode ? 'text-primary-100' : 'text-primary-800'
+            } mb-2`}
+          >
+            {project}
+          </h5>
+          <p
+            className={`text-xs ${paragraphText} mb-2`}
+          >{`${description.substring(0, 200)}...`}</p>
+          <div className="flex gap-1 flex-wrap">
+            {tags.length > 0 && tags.map(tag => <Tag key={tag} tag={tag} />)}
+          </div>
+        </div>
+      </Card>
+    </Link>
   );
 }
 
