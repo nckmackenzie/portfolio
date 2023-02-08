ba3f3c2c77a0c5cc6ce13f206dc734a72efee6db
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-08 15:44:14 +0300

    Footer component

---
+++ src/components/home/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import { AiOutlineArrowUp } from 'react-icons/ai';
+import { useTheme } from '../../../context/ThemeProvider';
+import { followMe } from '../../../data/data';
+import FollowLink from '../hero/FollowLink';
+
+function Footer() {
+  const { isDarkMode } = useTheme();
+  const onClickHandler = () => {
+    document.documentElement.scrollTop = 0;
+  };
+  return (
+    <div
+      className={`w-full ${
+        isDarkMode ? 'bg-slate-800' : 'bg-white'
+      } py-6 lg:py-8`}
+    >
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div
+            className={`hidden lg:block text-3xl ${
+              isDarkMode ? 'text-primary-200' : 'text-primary-800'
+            } font-black font-headings`}
+          >
+            Lets connect
+          </div>
+          <div className="flex gap-4">
+            {followMe?.length > 0 &&
+              followMe.map(link => <FollowLink key={link.name} {...link} />)}
+          </div>
+          <button
+            onClick={onClickHandler}
+            className={`text-sm flex items-center gap-1 ${
+              isDarkMode ? 'text-white' : 'text-slate-800'
+            }`}
+          >
+            <AiOutlineArrowUp />
+            <span>Back to top</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
