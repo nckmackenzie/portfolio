0e214d91a75a2eb31cc1f8b97630ffbc8255bfcd
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-08 11:49:20 +0300

    Reusable Alert component

---
+++ src/components/ui/Alert.jsx
@@ -0,0 +1,41 @@
+import { GrFormClose } from 'react-icons/gr';
+import React from 'react';
+import { useAlert } from '../../context/AlertProvider';
+
+function Alert({ status }) {
+  const { closeAlert } = useAlert();
+  const strongText =
+    status === 'success'
+      ? 'Success!!!'
+      : status === 'error'
+      ? 'Error!!!'
+      : 'Sending...';
+  const message =
+    status === 'success'
+      ? 'Your message has been sent successfully!'
+      : status === 'error'
+      ? 'There was a problem sending your message.Retry or try again later!'
+      : 'Please wait as your message is being sent!';
+  const bgColor =
+    status === 'success'
+      ? 'bg-green-300'
+      : status === 'error'
+      ? 'bg-red-300'
+      : 'bg-purple-300';
+  return (
+    <div
+      className={`w-full p-4 ${bgColor} rounded mb-2 flex items-center justify-between`}
+    >
+      <p>
+        <strong>{strongText}</strong> {message}
+      </p>
+      {(status === 'success' || status === 'error') && (
+        <button className="outline-none" onClick={closeAlert}>
+          <GrFormClose size={24} />
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Alert;
