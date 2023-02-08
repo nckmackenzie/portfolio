828b53dfec7eedcb4bf226275ca8e3cc470a4d91
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-08 11:45:33 +0300

    add named export for Alert component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,3 +2,4 @@ export { default as Card } from './Card';
 export { default as Button } from './Button';
 export { default as Section } from './Section';
 export { default as Rating } from './Rating';
+export { default as Alert } from './Alert';
