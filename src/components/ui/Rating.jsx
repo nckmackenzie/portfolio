22185a81af51a1d491e0ccd1e9e7a9df6aa8402c
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-08 10:16:52 +0300

    add key prop

--- src/components/ui/Rating.jsx
+++ src/components/ui/Rating.jsx
@@ -2,8 +2,8 @@ import { AiFillStar } from 'react-icons/ai';
 function Rating({ rating }) {
   return (
     <div className="text-primary-400 flex items-center gap-1">
-      {[...Array(rating)].map(rat => (
-        <AiFillStar />
+      {[...Array(rating)].map((_, i) => (
+        <AiFillStar key={i} />
       ))}
     </div>
   );
