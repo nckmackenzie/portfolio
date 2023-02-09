27917330b91f31ab1826f46c720e76bb7ec0cf8d
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-09 15:38:03 +0300

    remove Link wrapper for portfolio component

--- src/components/home/projects/Portfolio.jsx
+++ src/components/home/projects/Portfolio.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { useTheme } from '../../../context/ThemeProvider';
 import { Card } from '../../ui';
 import Tag from './Tag';
@@ -6,34 +5,30 @@ import Tag from './Tag';
 function Portfolio({ icon, project, description, tags, to }) {
   const { paragraphText, isDarkMode } = useTheme();
   return (
-    <Link to={`projects/${to}`} className="transition-all hover:scale-105">
-      <Card
-        className={`w-full ${isDarkMode ? 'bg-slate-800' : 'bg-slate-100'}`}
-      >
-        <div className="h-[9rem]">
-          <img
-            src={icon.small}
-            alt="Project description"
-            className="max-w-full h-full w-full"
-          />
+    <Card className={`w-full ${isDarkMode ? 'bg-slate-800' : 'bg-slate-100'}`}>
+      <div className="h-[9rem]">
+        <img
+          src={icon.small}
+          alt="Project description"
+          className="max-w-full h-full w-full"
+        />
+      </div>
+      <div className="p-2">
+        <h5
+          className={`${
+            isDarkMode ? 'text-primary-100' : 'text-primary-800'
+          } mb-2`}
+        >
+          {project}
+        </h5>
+        <p
+          className={`text-xs ${paragraphText} mb-2`}
+        >{`${description.substring(0, 200)}...`}</p>
+        <div className="flex gap-1 flex-wrap">
+          {tags.length > 0 && tags.map(tag => <Tag key={tag} tag={tag} />)}
         </div>
-        <div className="p-2">
-          <h5
-            className={`${
-              isDarkMode ? 'text-primary-100' : 'text-primary-800'
-            } mb-2`}
-          >
-            {project}
-          </h5>
-          <p
-            className={`text-xs ${paragraphText} mb-2`}
-          >{`${description.substring(0, 200)}...`}</p>
-          <div className="flex gap-1 flex-wrap">
-            {tags.length > 0 && tags.map(tag => <Tag key={tag} tag={tag} />)}
-          </div>
-        </div>
-      </Card>
-    </Link>
+      </div>
+    </Card>
   );
 }
 
