e0d65ac1963b459eb72de626f83e11bc8d9f9c63
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-10 08:39:07 +0300

    toggle sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,19 +1,23 @@
-import { BiCodeAlt } from 'react-icons/bi';
 import { GrClose } from 'react-icons/gr';
+import { useSideBar } from '../../context/SideBarProvider';
 import { useTheme } from '../../context/ThemeProvider';
 import { links } from '../../data/data';
 import NavItemMobile from './NavItemMobile';
 
 function Sidebar() {
   const { isDarkMode } = useTheme();
+  const { closeSideBar, sidebarIsOpen } = useSideBar();
   return (
     <aside
-      className={`sidebar ${
-        isDarkMode ? 'bg-slate-600' : 'bg-white'
-      } translate-x-0 translate-y-0`}
+      className={`sidebar ${isDarkMode ? 'bg-slate-600' : 'bg-white'} ${
+        sidebarIsOpen ? 'translate-x-0 translate-y-0' : ''
+      }`}
     >
       <div className="flex p-4 ">
-        <button className="text-lg text-primary-400 ml-auto">
+        <button
+          className="text-lg text-primary-400 ml-auto"
+          onClick={closeSideBar}
+        >
           <GrClose />
         </button>
       </div>
