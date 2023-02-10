42cacee8376f2138918412326f9a4941b92d878b
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-02-10 08:36:58 +0300

    close sidebar on link click

--- src/components/layout/NavItemMobile.jsx
+++ src/components/layout/NavItemMobile.jsx
@@ -1,7 +1,9 @@
+import { useSideBar } from '../../context/SideBarProvider';
 import { useTheme } from '../../context/ThemeProvider';
 
 function NavItemMobile({ name, to, icon }) {
   const { isDarkMode } = useTheme();
+  const { closeSideBar } = useSideBar();
   return (
     <li className="p-2 w-full">
       <a
@@ -9,6 +11,7 @@ function NavItemMobile({ name, to, icon }) {
         className={`flex items-center gap-4 ${
           isDarkMode ? 'text-slate-50' : 'text-slate-800'
         }`}
+        onClick={closeSideBar}
       >
         <span>{icon}</span>
         <span>{name}</span>
